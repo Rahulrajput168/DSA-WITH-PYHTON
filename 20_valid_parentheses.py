67897a53188a95ee5828f94fc7b7182926fdb4fb
Rahulrajput168 <rahulsinghrajput1843@gmail.com>
2026-02-24 10:18:53 +0530

    Add files via upload

---
+++ 20_valid_parentheses.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        m = {')': '(', '}': '{', ']': '['}
+        for c in s:
+            if c in m:
+                if not stack or stack.pop() != m[c]:
+                    return False
+            else:
+                stack.append(c)
+        return not stack
